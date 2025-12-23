9715b277e83054a3a79bf4b8489731b5885eaa32
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-23 15:37:26 +0000

    店铺状态

---
+++ firmament-server/src/main/java/dev/kaiwen/controller/user/ShopController.java
@@ -0,0 +1,32 @@
+package dev.kaiwen.controller.user;
+
+
+import dev.kaiwen.result.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Tag(name = "店铺相关接口-用户")
+@Slf4j
+@RequiredArgsConstructor
+public class ShopController {
+
+    private final RedisTemplate redisTemplate;
+    public static String KEY = "SHOP_STATUS";
+
+
+    @GetMapping("/status")
+    @Operation(summary = "获取店铺营业状态")
+    public Result<Integer> getStatus() {
+        Integer shopStatus =(Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取到店铺营业状态为 {}", shopStatus == 1 ? "营业中" : "打烊中");
+        return Result.success(shopStatus);
+    }
+
+
+}
