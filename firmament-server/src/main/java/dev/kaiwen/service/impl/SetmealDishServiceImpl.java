e4b9dea595b10588913e0002928e3d1de9ce65ff
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-23 12:25:43 +0000

    删除菜品

---
+++ firmament-server/src/main/java/dev/kaiwen/service/impl/SetmealDishServiceImpl.java
@@ -0,0 +1,13 @@
+package dev.kaiwen.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import dev.kaiwen.entity.SetmealDish;
+import dev.kaiwen.mapper.SetmealDishMapper;
+import dev.kaiwen.service.ISetmealDishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SetmealDishServiceImpl extends ServiceImpl<SetmealDishMapper, SetmealDish> implements ISetmealDishService {
+}
