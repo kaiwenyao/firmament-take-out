63ee66590e99f43eb0c6af8f756a24ded6ef0428
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-25 15:18:44 +0000

    订单查询 ws通信

--- firmament-server/src/main/java/dev/kaiwen/controller/user/OrderController.java
+++ firmament-server/src/main/java/dev/kaiwen/controller/user/OrderController.java
@@ -3,9 +3,11 @@ package dev.kaiwen.controller.user;
 
 import dev.kaiwen.dto.OrdersPaymentDTO;
 import dev.kaiwen.dto.OrdersSubmitDTO;
+import dev.kaiwen.result.PageResult;
 import dev.kaiwen.result.Result;
 import dev.kaiwen.service.IOrderService;
 import dev.kaiwen.vo.OrderSubmitVO;
+import dev.kaiwen.vo.OrderVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -36,6 +38,59 @@ public class OrderController {
         log.info("订单支付：{}", ordersPaymentDTO);
         orderService.payment(ordersPaymentDTO);
         return Result.success("支付成功");
+    }
+
+    /**
+     * 历史订单查询
+     *
+     * @param page
+     * @param pageSize
+     * @param status   订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消
+     * @return
+     */
+    @GetMapping("/historyOrders")
+    @Operation(summary = "历史订单查询")
+    public Result<PageResult> page(int page, int pageSize, Integer status) {
+        PageResult pageResult = orderService.pageQuery4User(page, pageSize, status);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 查询订单详情
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/orderDetail/{id}")
+    @Operation(summary = "查询订单详情")
+    public Result<OrderVO> details(@PathVariable("id") Long id) {
+        OrderVO orderVO = orderService.details(id);
+        return Result.success(orderVO);
+    }
+
+    /**
+     * 用户取消订单
+     *
+     * @return
+     */
+    @PutMapping("/cancel/{id}")
+    @Operation(summary = "取消订单")
+    public Result<String> cancel(@PathVariable("id") Long id) throws Exception {
+        orderService.userCancelById(id);
+        return Result.success();
+    }
+
+    /**
+     * 再来一单
+     *
+     * @param id
+     * @return
+     */
+    @PostMapping("/repetition/{id}")
+    @Operation(summary = "再来一单")
+    public Result<String> repetition(@PathVariable Long id) {
+        orderService.repetition(id);
+        return Result.success();
     }
 
 }
--- firmament-server/src/main/java/dev/kaiwen/service/IOrderService.java
+++ firmament-server/src/main/java/dev/kaiwen/service/IOrderService.java
@@ -1,10 +1,12 @@
 package dev.kaiwen.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import dev.kaiwen.dto.OrdersPaymentDTO;
-import dev.kaiwen.dto.OrdersSubmitDTO;
+import dev.kaiwen.dto.*;
 import dev.kaiwen.entity.Orders;
+import dev.kaiwen.result.PageResult;
+import dev.kaiwen.vo.OrderStatisticsVO;
 import dev.kaiwen.vo.OrderSubmitVO;
+import dev.kaiwen.vo.OrderVO;
 
 public interface IOrderService extends IService<Orders> {
     OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
@@ -20,4 +22,75 @@ public interface IOrderService extends IService<Orders> {
      * 处理前一天未完成的订单：将前一天的所有未完成订单标记为已完成
      */
     void processDeliveryOrder();
+    
+    /**
+     * 用户端订单分页查询
+     * @param page
+     * @param pageSize
+     * @param status
+     * @return
+     */
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+    
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    OrderVO details(Long id);
+    
+    /**
+     * 用户取消订单
+     * @param id
+     */
+    void userCancelById(Long id) throws Exception;
+    
+    /**
+     * 再来一单
+     * @param id
+     */
+    void repetition(Long id);
+    
+    /**
+     * 条件搜索订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+    
+    /**
+     * 各个状态的订单数量统计
+     * @return
+     */
+    OrderStatisticsVO statistics();
+    
+    /**
+     * 接单
+     * @param ordersConfirmDTO
+     */
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+    
+    /**
+     * 拒单
+     * @param ordersRejectionDTO
+     */
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
+    
+    /**
+     * 商家取消订单
+     * @param ordersCancelDTO
+     */
+    void cancel(OrdersCancelDTO ordersCancelDTO) throws Exception;
+    
+    /**
+     * 派送订单
+     * @param id
+     */
+    void delivery(Long id);
+    
+    /**
+     * 完成订单
+     * @param id
+     */
+    void complete(Long id);
 }
