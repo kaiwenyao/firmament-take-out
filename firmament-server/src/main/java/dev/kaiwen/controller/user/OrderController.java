1ce84b6af5f1cba00aeb8c42357a78467e39d167
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-25 13:35:02 +0000

    模拟支付成功

--- firmament-server/src/main/java/dev/kaiwen/controller/user/OrderController.java
+++ firmament-server/src/main/java/dev/kaiwen/controller/user/OrderController.java
@@ -1,6 +1,7 @@
 package dev.kaiwen.controller.user;
 
 
+import dev.kaiwen.dto.OrdersPaymentDTO;
 import dev.kaiwen.dto.OrdersSubmitDTO;
 import dev.kaiwen.result.Result;
 import dev.kaiwen.service.IOrderService;
@@ -9,10 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController("userOrderController")
@@ -30,6 +28,14 @@ public class OrderController {
         OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
         return Result.success(orderSubmitVO);
 
+    }
+
+    @PutMapping("/payment")
+    @Operation(summary = "订单支付")
+    public Result<String> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) {
+        log.info("订单支付：{}", ordersPaymentDTO);
+        orderService.payment(ordersPaymentDTO);
+        return Result.success("支付成功");
     }
 
 }
--- firmament-server/src/main/java/dev/kaiwen/service/IOrderService.java
+++ firmament-server/src/main/java/dev/kaiwen/service/IOrderService.java
@@ -1,10 +1,13 @@
 package dev.kaiwen.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import dev.kaiwen.dto.OrdersPaymentDTO;
 import dev.kaiwen.dto.OrdersSubmitDTO;
 import dev.kaiwen.entity.Orders;
 import dev.kaiwen.vo.OrderSubmitVO;
 
 public interface IOrderService extends IService<Orders> {
     OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+    
+    void payment(OrdersPaymentDTO ordersPaymentDTO);
 }
